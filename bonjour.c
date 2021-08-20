b53f074c2e680987022aa901612f112c1b7048a8
joeldevel <christianjoel.developer@gmail.com>
2021-08-20 01:38:33 -0300

    added details to bonjour.c

--- bonjour.c
+++ bonjour.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/* The hello world program in C */
-
+/* The hello world program in C 
+ * compile: $ gcc bonjour.c -o bonjour
+ * execute: $ ./bonjour
+ */
 int main() {
   printf("Bonjour mes amies!\n");
-  
   return 0;
 }

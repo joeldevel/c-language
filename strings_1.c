46963a1b2cd8bf8e904db2063a9ee9384fa6ebfa
joeldevel <christianjoel.developer@gmail.com>
2021-08-29 18:09:29 -0300

    string example

---
+++ strings_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * compile: $ gcc strings_1.c -o strings_1 
+ * run    : $ ./strings_1 
+ */
+int main() {
+    char* a_string = "Aujourd'hui il fait beau\n";
+
+    printf("%s", a_string);
+
+    return 0;
+}

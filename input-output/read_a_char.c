99ee43c1d0acba55958bbf32b4a80858463bfe52
joeldevel <christianjoel.developer@gmail.com>
2021-09-30 23:42:49 -0300

    add reading a char from stdin using getc example

---
+++ input-output/read_a_char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char user_input = ' ';
+    printf("enter a char: >>> ");
+
+    user_input = getchar();
+
+    printf("you've entered %c\n", user_input);
+
+
+    return 0;
+}

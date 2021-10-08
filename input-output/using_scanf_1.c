3cb5e8862e89117e8c81225cbbb2d5986efad621
joeldevel <christianjoel.developer@gmail.com>
2021-10-08 00:11:02 -0300

    add scanf example to read a number

---
+++ input-output/using_scanf_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int user_input;
+    printf("Enter an integer number: ");
+    scanf("%d", &user_input);
+    printf("\nYou entered: %d\n", user_input);
+
+    return 0;
+}
